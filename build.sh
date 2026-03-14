e37f0ca22ddcb90e82e6b52dea284466f24a8944
korbykob <korbykobs@gmail.com>
2026-03-14 05:09:19 +0000

    Add removed compile args

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 
 mkdir -p bin
-gcc $PROGRAM_COMPILER_FLAGS -Iinclude src/terminal.c -o bin/terminal.o
+gcc $PROGRAM_COMPILER_FLAGS -Iinclude -g -fno-omit-frame-pointer -mavx2 -O2 -fvect-cost-model=dynamic -Wall -Wextra -Werror src/terminal.c -o bin/terminal.o
 nm bin/terminal.o > bin/terminal.sym
 ld $PROGRAM_LINKER_FLAGS bin/terminal.o -o bin/terminal.nxe
