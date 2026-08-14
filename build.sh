aa8a1b9ecc838e384e0a224159afa7569ae6f908
korbykob <korbykobs@gmail.com>
2026-08-14 18:10:14 +0100

    Allow compilation from other architectures

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 
 mkdir -p bin
-gcc $PROGRAM_COMPILER_FLAGS -isystem sys -g -fno-omit-frame-pointer -O2 -fvect-cost-model=dynamic -Wall -Wextra -Werror src/terminal.c -o bin/terminal.o
-nm bin/terminal.o > bin/terminal.sym
-ld $PROGRAM_LINKER_FLAGS bin/terminal.o -o bin/terminal.nxe
+x86_64-linux-gnu-gcc $PROGRAM_COMPILER_FLAGS -isystem sys -g -fno-omit-frame-pointer -O2 -fvect-cost-model=dynamic -Wall -Wextra -Werror src/terminal.c -o bin/terminal.o
+x86_64-linux-gnu-nm bin/terminal.o > bin/terminal.sym
+x86_64-linux-gnu-ld $PROGRAM_LINKER_FLAGS bin/terminal.o -o bin/terminal.nxe
